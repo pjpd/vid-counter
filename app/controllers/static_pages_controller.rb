183a2cdd5f8cd5bb4e1eb9a7c0c658d2205da503
Paul Dunn <pauljosephdunn@gmail.com>
2017-03-06 14:49:38 +1300

    add basic page scraping with mechanize

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,5 +4,8 @@ class StaticPagesController < ApplicationController
 
   def submit
     @video_url = params[:website_url]
+    agent = Mechanize.new
+    agent.get @video_url
+    @view_count = agent.page.at(".watch-view-count").text.split(" ")[0]
   end
 end
