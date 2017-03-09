44cc13d434dba259257bfa5aed5ea3e8c29b0168
Paul Dunn <pauljosephdunn@gmail.com>
2017-03-10 12:51:36 +1300

    send barry somewhere odd with this videos because he's a pain in the a$$

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,11 +3,18 @@ class StaticPagesController < ApplicationController
   end
 
   def submit
-    @video_url = params[:website_url]
-    agent = Mechanize.new
-    agent.get @video_url
+    begin
+      @video_url = params[:website_url]
+      agent = Mechanize.new
+      agent.get @video_url
 
-    @view_count = agent.page.at(".watch-view-count").text.split(" ")[0]
-    @video_title = agent.page.at("//title").text.split(" - ")[0]
+      @view_count = agent.page.at(".watch-view-count").text.split(" ")[0]
+      @video_title = agent.page.at("//title").text.split(" - ")[0]
+    rescue
+      # If someone submits a non-youtube url, the above is likely to fail.
+      # That person is probably Barry.
+      # Send him somewhere not on this site.
+      redirect_to "https://www.gsb.stanford.edu/faculty-research/working-papers/defining-minimum-winning-game-high-technology-ventures"
+    end
   end
 end
